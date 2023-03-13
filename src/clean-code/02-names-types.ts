bcf62019797e5ff7e1083d7f58cef6488bb9ee92
AndrusGerman <andrusdiazaleman@gmail.com>
2023-03-13 18:32:37 -0400

    remove comments

--- src/clean-code/02-names-types.ts
+++ src/clean-code/02-names-types.ts
@@ -1,28 +1,22 @@
 (() => {
-  // arreglo de temperaturas celsius
   const temperaturesCelsius = [33.6, 12.34];
 
-  // Dirección ip del servidor
-  const clientIp = "123.123.123.123";
+  const serverIp = "123.123.123.123";
 
-  // Listado de usuarios
   const users = [
     { id: 1, email: "fernando@google.com" },
     { id: 2, email: "juan@google.com" },
     { id: 3, email: "melissa@google.com" },
   ];
 
-  // Listado de emails de los usuarios
   const emails = users.map((user) => user.email);
 
-  // Variables booleanas de un video juego
   const canJump = false;
   const canRun = true;
   const hasItems = true;
   const isLoading = false;
 
-  // Otros ejercicios
-  // tiempo inicial
+
   const initialTime = new Date().getTime();
   //....
   // 3 doritos después
@@ -30,7 +24,6 @@
   // Tiempo al final
   const endTime = new Date().getTime() - initialTime;
 
-  // Funciones
   // Obtiene los libros
   function getBooks() {
     throw new Error("Function not implemented.");
