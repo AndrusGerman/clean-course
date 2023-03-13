eeced316239fec2fe84896ccb15ebcf7d8a1b293
AndrusGerman <andrusdiazaleman@gmail.com>
2023-03-13 19:01:07 -0400

    getPayAmount.ts exercise

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,44 +1,77 @@
 (() => {
+  // función para obtener información de una película por Id
+  function getMovieByID(movieId: string) {
+    console.log({ movieId });
+  }
 
-    // función para obtener información de una película por Id
-    function getMovieByID( movieId: string ) {
-        console.log({ movieId });
+  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+  function getMovieCast(movieID: string) {
+    console.log({ movieID });
+  }
+
+  // funcion para obtener el bio del actor por el id
+  function getActorBioByID(ActorId: string) {
+    console.log({ ActorId });
+  }
+
+  // Crear una película
+  interface Movie {
+    cast?: string[];
+    description: string;
+    rating?: number;
+    title: string;
+  }
+  function createMovie({ title, description, rating, cast }: Movie) {
+    console.log({ title, description, rating, cast });
+  }
+
+  function checkFullName(fullName: string): boolean {
+    return fullName != "fernando";
+  }
+  function createActor(fullName: string, birthdate: Date): boolean {
+    if (checkFullName(fullName)) return false;
+
+    console.log("Crear actor");
+    return true;
+  }
+
+  const getPayAmountOld = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }) => {
+    let result;
+    if (isDead) {
+      result = 1500;
+    } else {
+      if (isSeparated) {
+        result = 2500;
+      } else {
+        if (isRetired) {
+          result = 3000;
+        } else {
+          result = 4000;
+        }
+      }
     }
 
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getMovieCast( movieID: string ) {
-        console.log({ movieID });
-    }
+    return result;
+  };
 
-    // funcion para obtener el bio del actor por el id
-    function getActorBioByID( ActorId: string ) {
-        console.log({ ActorId });
-    }
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }) => {
     
-    // Crear una película
-    interface Movie {
-        cast?: string[]
-        description: string, 
-        rating?: number, 
-        title: string, 
-    }
-    function createMovie({title, description, rating, cast}:Movie ) {
-        console.log({ title, description, rating, cast });
+    if (isDead) {
+      return 1500;
     }
 
-
-
-    function checkFullName(fullName:string):boolean {
-            return fullName!="fernando";
-    }
-    function createActor( fullName: string, birthdate: Date ): boolean {
-        if (checkFullName(fullName)) return false;
-
-        console.log('Crear actor');
-        return true;        
+    if (isSeparated) {
+      return 2500;
     }
 
-    
-
-
+    return isRetired ? 3000 : 4000;
+  };
 })();
