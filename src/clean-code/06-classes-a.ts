965ba7987e7646b8ccde376572a9c8e4f29be77b
AndrusGerman <andrusdiazaleman@gmail.com>
2023-03-15 18:23:05 -0400

    herencia problematica

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -12,4 +12,48 @@
   const newPerson = new Person("Fernando", "M", new Date("2001-05-15"));
 
   console.log(newPerson);
+
+  class User extends Person {
+    private lastAccess: Date;
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public WorkingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(email, role, name, gender, birthdate);
+    }
+  }
+
+  const userSettings = new UserSettings(
+    "/usr/home",
+    "/home",
+    "andrusdiazaleman@gmail.com",
+    "Admin",
+    "Andrus diaz",
+    "M",
+    new Date("2001-05-15")
+  );
+
+  console.log(userSettings);
 })();
