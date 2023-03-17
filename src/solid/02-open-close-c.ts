740b3ac3cd965e79d2464dc3db5f7864cbd494c9
AndrusGerman <andrusdiazaleman@gmail.com>
2023-03-17 14:33:01 -0400

    change axios to fetch

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,13 +1,18 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 interface httpClientParams {
-    url:string
+  url: string;
 }
 
 export class HttpClient {
-    async get({url}:httpClientParams){
-        const {data, status} = await axios.get(url);
-        return {data,status};
-        
-    }
+  // async get({url}:httpClientParams){
+  //     const {data, status} = await axios.get(url);
+  //     return {data,status};
+  // }
+
+  async get({ url }: httpClientParams) {
+    const resp = await fetch(url);
+    const data = resp.json();
+    return { data, status: resp.status };
+  }
 }
