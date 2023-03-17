7f138c036b74c26fe1d61e14417e5df08f3e2eb9
AndrusGerman <andrusdiazaleman@gmail.com>
2023-03-17 12:12:35 -0400

    format coupling

--- src/code-smells/02-high-coupling.ts
+++ src/code-smells/02-high-coupling.ts
@@ -1,69 +1,58 @@
-(()=>{
-    // No aplicando el principio de responsabilidad única
-    type Gender = 'M'|'F';
+(() => {
+  // No aplicando el principio de responsabilidad única
+  type Gender = "M" | "F";
 
-    // Alto Acoplamiento
+  // Alto Acoplamiento
 
-    class Person {
-        constructor(
-            public name: string,
-            public gender: Gender,
-            public birthdate: Date,
-        ){}
-    }
-
-    class User extends Person {
-        constructor(
-            public email: string,
-            public role: string,
-            private lastAccess: Date,
-            name: string,
-            gender: Gender,
-            birthdate: Date,
-        ){
-            super(name, gender, birthdate);
-            this.lastAccess = new Date();
-        }
-
-        checkCredentials() {
-            return true;
-        }
-    }
-
-
-class UserSettings extends User {
+  class Person {
     constructor(
-        public workingDirectory: string,
-        public lastFolderOpen: string,
-        email     : string,
-        role      : string,
-        name      : string,
-        gender    : Gender,
-        birthdate : Date,
-    ){
-        super(
-            email,
-            role,
-            new Date(),
-            name,
-            gender,
-            birthdate
-        )
+      public name: string,
+      public gender: Gender,
+      public birthdate: Date
+    ) {}
+  }
+
+  class User extends Person {
+    constructor(
+      public email: string,
+      public role: string,
+      private lastAccess: Date,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
     }
-}
-    
 
+    checkCredentials() {
+      return true;
+    }
+  }
 
-    const userSettings = new UserSettings(
-        '/urs/home',
-        '/development',
-        'fernando@google.com',
-        'F',
-        'Fernando',
-        'M',
-        new Date('1985-10-21')
-    );
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastFolderOpen: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(email, role, new Date(), name, gender, birthdate);
+    }
+  }
 
-    console.log({ userSettings, credentials: userSettings.checkCredentials() });
-    
-})()
+  const userSettings = new UserSettings(
+    "/urs/home",
+    "/development",
+    "fernando@google.com",
+    "F",
+    "Fernando",
+    "M",
+    new Date("1985-10-21")
+  );
+
+  console.log({ userSettings, credentials: userSettings.checkCredentials() });
+})();
